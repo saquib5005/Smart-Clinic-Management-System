d901f7cc952f863e89a9a1f2cac3f2a5397faeb5
Saquib Khan <111189836+saquib5005@users.noreply.github.com>
2025-08-31 10:57:36 +0530

    Create Appointment.java

---
+++ Appointment.java
@@ -0,0 +1,19 @@
+@Entity
+public class Appointment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long appointmentId;
+
+    @ManyToOne
+    @JoinColumn(name = "doctor_id")
+    private Doctor doctor;
+
+    @ManyToOne
+    @JoinColumn(name = "patient_id")
+    private Patient patient;
+
+    @Future
+    private LocalDateTime appointmentTime;
+
+    // Getters and Setters
+}
